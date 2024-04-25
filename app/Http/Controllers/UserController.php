e9390aeb166fa3712b9fb4da755a74af0b5e85a9
muhammadshiva <shivamuhammad07@gmail.com>
2024-04-25 10:48:30 +0700

    add update user function

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -40,4 +40,54 @@ class UserController extends Controller
 
         return response()->json(['message' => 'User registered successfully'], 200);
     }
+
+    public function update(Request $request){
+        $user = Auth::user();
+
+        $request->validate([
+            'name' => 'string',
+            'email' => 'email|unique:users,email,'.$user->id,
+            'username' => 'string|unique:users,username,'.$user->id,
+            'password' => 'string|min:6',
+            'position' => 'string',
+            'phone' => 'string',
+            'profile_picture' => 'image|mimes:jpeg,png,jpg,gif|max:2048',
+        ]);
+
+        if ($request->has('name')) {
+            $user->name = $request->name;
+        }
+
+        if ($request->has('email')) {
+            $user->email = $request->email;
+        }
+
+        if ($request->has('username')) {
+            $user->username = $request->username;
+        }
+
+        if ($request->has('password')) {
+            $user->password = bcrypt($request->password);
+        }
+
+        if ($request->has('position')) {
+            $user->position = $request->position;
+        }
+
+        if ($request->has('phone')) {
+            $user->phone = $request->phone;
+        }
+
+        if ($request->hasFile('profile_picture')) {
+            $image = $request->file('profile_picture');
+            $imageName = time().'.'.$image->extension();
+            $image->move(public_path('images'), $imageName);
+            $user->profile_picture = $imageName;
+        }
+
+        $user->save();
+
+        return response()->json(['message' => 'User updated successfully'], 200);
+    }
+
 }
