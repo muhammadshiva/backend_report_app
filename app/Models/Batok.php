34ef574cce970b611230746a594dad31cb930d4e
muhammadshiva <shivamuhammad07@gmail.com>
2024-04-25 09:54:41 +0700

    add relation to user tabel

--- app/Models/Batok.php
+++ app/Models/Batok.php
@@ -4,8 +4,25 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use App\Models\User;
 
 class Batok extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'id_user',
+        'tanggal',
+        'sumber_batok',
+        'barang_masuk',
+        'barang_keluar',
+        'stok_awal',
+        'stok_akhir',
+        'keterangan',
+    ];
+
+    public function user(){
+        return $this->belongsTo(User::class, 'id_user');
+    }
+
 }
