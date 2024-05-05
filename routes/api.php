f69f86ce855b6aed1f69f221bee9600264827638
muhammadshiva <shivamuhuammad07@gmail.com>
2024-05-05 23:17:43 +0700

    delete unused code

--- routes/api.php
+++ routes/api.php
@@ -26,11 +26,7 @@ Route::post('login', [AuthController::class, 'login']);
 Route::post('is_email_exist', [UserController::class, 'isEmailExist']);
 
 Route::group(['middleware' => 'jwt.verify'], function ($router) {
-    Route::get('test', function(Request $request){
-        return 'success';
-    });
     Route::get('users', [UserController::class, 'show']);
     Route::get('users/{username}', [UserController::class, 'getUserByUsername']);
     Route::put('users', [UserController::class, 'update']);
-
 });
